bd337a739b85c812d3bf09df87fe55b28eb3ef4c
Richy <richymbiamou52@gmail.com>
2023-02-26 11:44:49 +0100

    fin du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Recollection';
+
+  ngOnInit(){}
   set = setInterval(() =>{
     let img = document.querySelector('.main_bg') as HTMLImageElement;
     let attr: string;
     if (img.getAttribute('src') == "assets/1.png"){
-      attr = "assets/2.png"
+      attr = "assets/2.png";
     }else if (img.getAttribute('src') == "assets/2.png"){
-      attr = "assets/3.png"
+      attr = "assets/3.png";
     }else if (img.getAttribute('src') == "assets/3.png"){
-      attr = "assets/4.png"
+      attr = "assets/4.png";
     }else {
-      attr = "assets/1.png"
+      attr = "assets/1.png";
     }
-    console.log(attr)
     img.setAttribute('src', attr);
   }, 3000)
+
 
 
 }
