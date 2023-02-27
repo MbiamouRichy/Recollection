ac9c345bb46466439fa8efa0020f596e52c1304b
Richy <richymbiamou52@gmail.com>
2023-02-27 03:10:37 +0100

    fin du projet amelioration des animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Recollection';
 
-  ngOnInit(){}
-  set = setInterval(() =>{
-    let img = document.querySelector('.main_bg') as HTMLImageElement;
-    let attr: string;
-    if (img.getAttribute('src') == "assets/1.png"){
-      attr = "assets/2.png";
-    }else if (img.getAttribute('src') == "assets/2.png"){
-      attr = "assets/3.png";
-    }else if (img.getAttribute('src') == "assets/3.png"){
-      attr = "assets/4.png";
-    }else {
-      attr = "assets/1.png";
-    }
-    img.setAttribute('src', attr);
-  }, 3000)
-
+  ngOnInit(){
+      let img = document.querySelector('.main_bg') as HTMLImageElement;
+       img.addEventListener('animationiteration', () =>{
+         let attr: string;
+         if (img.getAttribute('src') == "assets/1.png"){
+           attr = "assets/2.png";
+         }else if (img.getAttribute('src') == "assets/2.png"){
+           attr = "assets/3.png";
+         }else if (img.getAttribute('src') == "assets/3.png"){
+           attr = "assets/4.png";
+         }else {
+           attr = "assets/1.png";
+         }
+         img.setAttribute('src', attr);
+       })
+  }
 
 
 }
